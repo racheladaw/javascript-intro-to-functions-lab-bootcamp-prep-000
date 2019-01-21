e5495d2c084ee41dee2a8e6787acd75d8373401c
Rachel Williams <williamsrachelada@gmail.com>
2019-01-21 02:38:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ function sayHiToGrandma(string) {
     return 'I can\'t hear you!'
 } else if (string === string.toUpperCase()) {
     return 'YES INDEED!'
-} else if (string === "I love ")
+} else if (string === "I love you, Grandma.") {
+    return 'I love you, too.'
+}
 
 }
